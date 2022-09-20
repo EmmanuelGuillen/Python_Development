34c1dc431cce8de6157ed77cae079e65ccf53151
EmmanuelGuillen <112149306+EmmanuelGuillen@users.noreply.github.com>
2022-09-19 21:16:39 -0700

    Delete 2dLists.py

--- 2dLists.py
+++
@@ -1,8 +0,0 @@
-# 2D lists = a list of lists
-
-drinks = ["soda", "beer", "lemonade"]
-dinner = ["steak", "hamburger", "pasta"]
-dessert = ["ice cream", "jello"]
-
-food = [drinks, dinner, dessert]
-print(food[1][2])
