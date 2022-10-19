15de1767e557164fe4d559860632780024b653b9
EmmanuelGuillen <112149306+EmmanuelGuillen@users.noreply.github.com>
2022-10-18 20:06:51 -0700

    indexOperator
    Gives access to a sequence's elements

---
+++ indexing.py
@@ -0,0 +1,14 @@
+# Index operator [] = gives access to a sequence's elements (str, list, tuples)
+
+name = "manny alvarez!"
+
+#if(name[0].islower()):
+#    name = name.capitalize()
+
+first_name = name[:5].upper()
+last_name = name[6:].lower()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
