724e58c19aceaba5fc146a3e549ffd131042e737
EmmanuelGuillen <112149306+EmmanuelGuillen@users.noreply.github.com>
2022-12-05 22:01:29 -0800

    typeCasting
    Simple way to lower the lines of code being used to produce the same outcome.

---
+++ typeCasting.py
@@ -0,0 +1,13 @@
+# type casting = convert the data type of a value to another data type.
+
+x = 1 #that would be a whole integer or int for short
+y = 2.0 #this would be a floating point number or float for short
+z = "3" # this would be a string or str for short
+
+# y = int(y) # to convert a float into int
+# z = int(z) # to convert string into int
+
+
+print(x)
+print(int(y))
+print(int(z))
