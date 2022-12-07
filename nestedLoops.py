41c7b44cf2c4d3e7f4b794fdc035c42375ee1201
EmmanuelGuillen <112149306+EmmanuelGuillen@users.noreply.github.com>
2022-12-06 22:52:33 -0800

    Update nestedLoops.py

--- nestedLoops.py
+++ nestedLoops.py
@@ -8,4 +8,4 @@ symbol = input("Enter symbol to use: ")
 for i in range(rows):
     for j in range(columns):
         print(symbol, end="")
-    print()
+    print()        # make sure this print statement is not in the inner loop which is why its indented only once not twice
